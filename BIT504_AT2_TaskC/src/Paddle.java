e79e81fd94eccbcfba3cd5d330aecc79c27d4e11
jm-hay <109257988+jm-hay@users.noreply.github.com>
2022-08-29 20:05:58 +0900

    Update Paddle.java
    Line 5: "PADDLE_HEIGHT = 100" changed to "PADDLE_HEIGHT = 50".
    Line 6: "PADDLE_COLOUR = Color.WHITE" changed to "PADDLE_COLOUR = Color.RED".
    Line 7: "DISTANCE_FROM_EDGE = 40" changed to "DISTANCE_FROM_EDGE = 20".

--- BIT504_AT2_TaskC/src/Paddle.java
+++ BIT504_AT2_TaskC/src/Paddle.java
@@ -3,9 +3,9 @@ import java.awt.Color;
 
 public class Paddle extends Sprite {
 	private static final int PADDLE_WIDTH = 10;
-	private static final int PADDLE_HEIGHT = 100;
-	private static final Color PADDLE_COLOUR = Color.WHITE;
-	public static final int DISTANCE_FROM_EDGE = 40;
+	private static final int PADDLE_HEIGHT = 50;
+	private static final Color PADDLE_COLOUR = Color.RED;
+	public static final int DISTANCE_FROM_EDGE = 20;
 	
 	public Paddle(Player player, int panelWidth, int panelHeight) {
 		setWidth(PADDLE_WIDTH);
